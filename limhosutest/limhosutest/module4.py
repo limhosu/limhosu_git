0e5ae69834cf06b4ece1f9fd7d4491a5afad8c65
limhosu <limhosu67@gmail.com>
2021-01-11 23:00:03 +0900

    limhosumodule4

--- limhosutest/limhosutest/module4.py
+++ limhosutest/limhosutest/module4.py
@@ -4,7 +4,7 @@ class Person:
 
 class Student(Person):
     def greeting(self):
-        print('안녕하세요. 저는 파이썬 코딩 도장 임호수입니다.')
+        print('안녕하세요. 저는 파이썬 코딩 도장 임재환입니다.')
 
 james=Student()
 james.greeting()
