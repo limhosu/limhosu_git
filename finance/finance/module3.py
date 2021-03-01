6f1e98a6ebb0fb65a922ee628c535f4c285d5628
limhosu <limhosu67@gmail.com>
2021-03-01 21:49:04 +0900

    20210301

--- finance/finance/module3.py
+++ finance/finance/module3.py
@@ -1,3 +1,17 @@
 import FinanceDataReader as fdr
-df = fdr.DataReader('001040', '2011-04-25', '2020-12-30')
-print(df.head(10))
+import pandas as pd
+import FinanceDataReader as fdr
+import seaborn as sns
+import numpy as np
+#df = fdr.DataReader('001040', '2011-04-25', '2020-12-30')
+#print(df.head(10))
+
+
+# 삼성전자(005930) 전체 (1996-11-05 ~ 현재)
+
+df = fdr.DataReader('005930', '2020-01-01', '2020-02-15')
+
+fdr.chart.plot(df)
+fdr.chart.plot(df, title='삼성전자(005930)')
+print(df)
+
--- limhosutest/limhosutest/module4.py
+++ limhosutest/limhosutest/module4.py
@@ -1,5 +1,16 @@
-def add(a,b):
-    return a + b
-
-def mul(a,b):
-    return a * b
+from openpyxl import load_workbook
+import datetime
+from datetime import date
+from datetime import timedelta
+import pandas as pd
+import numpy as np
+import csv
+import os
+wb1 = "d:/limhosu/limhosutest/limhosutest/test1.xlsx"
+data1 = pd.read_excel(wb1)                      
+print(data1)
+wb2 = "d:/limhosu/limhosutest/limhosutest/test2.xlsx"
+data2 = pd.read_excel(wb2)
+print(data2)
+dataall = data1.merge(data2,how='left', on=['날짜', '동','호수'])
+print(dataall)
